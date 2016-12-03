588278d77aeb3b6cda44244e86442fcf9e4709b2
Ken Sodemann <ken.sodemann@teampenta.com>
2016-12-03 14:41:20 -0600

    refactor(system): change local storage library

--- src/app/authentication/shared/authentication-token.service.spec.ts
+++ src/app/authentication/shared/authentication-token.service.spec.ts
@@ -1,11 +1,17 @@
 import { TestBed, inject } from '@angular/core/testing';
 import { AuthenticationTokenService } from './authentication-token.service';
-import { LocalStorage } from 'h5webstorage';
+import { LocalStorageService, LOCAL_STORAGE_SERVICE_CONFIG } from 'angular-2-local-storage';
 
 describe('AuthenticationToken Service', () => {
+  let localStorageConfig = {};
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [AuthenticationTokenService, {provide: LocalStorage, useClass: MockLocalStorage}]
+      providers: [
+        AuthenticationTokenService,
+        LocalStorageService,
+        { provide: LOCAL_STORAGE_SERVICE_CONFIG, useValue: localStorageConfig }
+      ]
     });
   });
 
@@ -16,28 +22,26 @@ describe('AuthenticationToken Service', () => {
 
   describe('setting a token', () => {
     it('saves the token to local storage',
-      inject([LocalStorage, AuthenticationTokenService], (localStorage : LocalStorage, service: AuthenticationTokenService) => {
+      inject([LocalStorageService, AuthenticationTokenService], (localStorage: LocalStorageService, service: AuthenticationTokenService) => {
+        spyOn(localStorage, 'set')
         service.setToken('IAmACuteLittleLoginToken');
-        expect(localStorage['authenticationToken']).toEqual('IAmACuteLittleLoginToken');
+        expect(localStorage.set).toHaveBeenCalledTimes(1);
+        expect(localStorage.set).toHaveBeenCalledWith('authenticationToken', 'IAmACuteLittleLoginToken');
       }));
   });
 
   describe('getting a token', () => {
     it('gets the token from local storage',
-      inject([LocalStorage, AuthenticationTokenService], (localStorage: LocalStorage, service: AuthenticationTokenService) => {
-        localStorage['authenticationToken'] = 'ThisIsTheToken'
+      inject([LocalStorageService, AuthenticationTokenService], (localStorage: LocalStorageService, service: AuthenticationTokenService) => {
+        service.setToken('ThisIsTheToken');
         expect(service.getToken()).toEqual('ThisIsTheToken');
       }));
 
     it('gets the last set token',
-      inject([LocalStorage, AuthenticationTokenService], (localStorage: LocalStorage, service: AuthenticationTokenService) => {
-        localStorage['authenticationToken'] = 'ThisIsTheToken'
+      inject([LocalStorageService, AuthenticationTokenService], (localStorage: LocalStorageService, service: AuthenticationTokenService) => {
+        service.setToken('ThisIsTheToken');
         service.setToken('ThisIsANewToken');
         expect(service.getToken()).toEqual('ThisIsANewToken');
       }));
   });
-  
-  class MockLocalStorage {
-    authenticationToken: string
-  }
 });
--- src/app/authentication/shared/authentication-token.service.ts
+++ src/app/authentication/shared/authentication-token.service.ts
@@ -1,16 +1,17 @@
 import { Injectable } from '@angular/core';
-import { LocalStorage } from 'h5webstorage';
+import { LocalStorageService } from 'angular-2-local-storage';
 
 @Injectable()
 export class AuthenticationTokenService {
+  private key = 'authenticationToken';
 
-  constructor(private localStorage: LocalStorage) { }
+  constructor(private localStorage: LocalStorageService) { }
 
   setToken(token: string) {
-    this.localStorage['authenticationToken'] = token;
+    this.localStorage.set(this.key, token);
   }
 
   getToken(): string {
-    return this.localStorage['authenticationToken'];
+    return this.localStorage.get<string>(this.key);
   }
 }
