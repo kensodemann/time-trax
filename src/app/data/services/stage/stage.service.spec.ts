190592f37bdc9eabd6580822834942e877090d95
Ken Sodemann <ken.sodemann@teampenta.com>
2016-12-11 08:00:11 -0600

    feat(data): add the stage service

---
+++ src/app/data/services/stage/stage.service.spec.ts
@@ -0,0 +1,113 @@
+import { Http, Response, ResponseOptions, RequestMethod, BaseRequestOptions } from '@angular/http';
+import { MockBackend, MockConnection } from '@angular/http/testing';
+
+import { StageService } from './stage.service';
+import { environment } from '../../../../environments/environment';
+
+describe('StageService', () => {
+  let service;
+
+  let http;
+  let mockBackend;
+
+  beforeEach(() => {
+    let opt = new BaseRequestOptions();
+    mockBackend = new MockBackend();
+    http = new Http(mockBackend, opt);
+
+    service = new StageService(http);
+  });
+
+  it('exists', () => {
+    expect(service).toBeTruthy();
+  });
+
+  describe('getAll', () => {
+    it('gets all of the stages', () => {
+      let connection: MockConnection;
+      mockBackend.connections.subscribe(c => connection = c);
+
+      let result;
+      service.getAll().subscribe(res => result = res);
+      expect(connection.request.url).toEqual(`${environment.dataService}/stages`);
+      expect(connection.request.method).toEqual(RequestMethod.Get);
+      connection.mockRespond(new Response(new ResponseOptions({
+        status: 200,
+        body: [{
+          _id: '42',
+          stageNumber: 1,
+          name: 'Deep Thought'
+        }, {
+          _id: '1138',
+          stageNumber: 2,
+          name: 'Drackonian Crackdown'
+        }, {
+          _id: '314159',
+          stageNumber: 3,
+          name: 'Eat Pie'
+        }]
+      })));
+      expect(result).toEqual([{
+        _id: '42',
+        stageNumber: 1,
+        name: 'Deep Thought'
+      }, {
+        _id: '1138',
+        stageNumber: 2,
+        name: 'Drackonian Crackdown'
+      }, {
+        _id: '314159',
+        stageNumber: 3,
+        name: 'Eat Pie'
+      }]);
+    });
+
+    it('caches the result so stages are not fetched more than once', () => {
+      let connection: MockConnection;
+      let connectionCount: number = 0;
+      mockBackend.connections.subscribe((c) => {
+        connection = c;
+        connectionCount++;
+      });
+
+      let result;
+
+      service.getAll();
+      connection.mockRespond(new Response(new ResponseOptions({
+        status: 200,
+        body: [{
+          _id: '42',
+          stageNumber: 1,
+          name: 'Deep Thought'
+        }, {
+          _id: '1138',
+          stageNumber: 2,
+          name: 'Drackonian Crackdown'
+        }, {
+          _id: '314159',
+          stageNumber: 3,
+          name: 'Eat Pie'
+        }]
+      })));
+
+      for (let x = 0; x < 100; x++) {
+        service.getAll().subscribe(res => result = res);
+      }
+      
+      expect(connectionCount).toEqual(1);
+      expect(result).toEqual([{
+        _id: '42',
+        stageNumber: 1,
+        name: 'Deep Thought'
+      }, {
+        _id: '1138',
+        stageNumber: 2,
+        name: 'Drackonian Crackdown'
+      }, {
+        _id: '314159',
+        stageNumber: 3,
+        name: 'Eat Pie'
+      }]);
+    });
+  });
+});
