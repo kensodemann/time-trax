5ec9113b2cfc9fc15afec682176e37b80454ebf1
Ken Sodemann <ken.sodemann@teampenta.com>
2016-12-19 05:45:39 -0600

    fix(tests): cover all paths in the code

--- src/app/+timesheet-history/timesheet-history.component.spec.ts
+++ src/app/+timesheet-history/timesheet-history.component.spec.ts
@@ -10,9 +10,15 @@ describe('Component: Timesheet History', () => {
     });
   });
 
-  it('should create the component', async(() => {
+  it('builds', async(() => {
     let fixture = TestBed.createComponent(TimesheetHistoryComponent);
     let app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
+  }));
+
+  it('implements OnInit', async(() => {
+    let fixture = TestBed.createComponent(TimesheetHistoryComponent);
+    let app = fixture.debugElement.componentInstance;
+    app.ngOnInit();
   }));
 });
